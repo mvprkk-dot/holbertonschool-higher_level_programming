9d143b1717be75a7342102df6663c0628696383e
mvprkk-dot <limurad42@gmail.com>
2026-04-12 14:42:59 +0000

    Final fix for Task 14: Proper all-state output and no indentation errors

--- python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Prints all City objects from the database based on State name."""
+"""Lists all City objects from the database."""
 import sys
 from model_state import Base, State
 from model_city import City
@@ -8,15 +8,17 @@ from sqlalchemy.orm import sessionmaker
 
 
 if __name__ == "__main__":
-    u, p, d, s_name = sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4]
+    # Arqumentləri birbaşa götürürük, heç bir sabit parol yazmırıq
+    u, p, d = sys.argv[1], sys.argv[2], sys.argv[3]
     uri = 'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(u, p, d)
     engine = create_engine(uri, pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    res = session.query(City).join(State).filter(State.name == s_name).order_by(City.id).all()
+    # Bütün sorğunu tək sətirdə yazırıq ki, E127 xətası fiziki olaraq QEYRİ-MÜMKÜN olsun
+    res = session.query(City, State).join(State, City.state_id == State.id).order_by(City.id).all()
 
-    for city in res:
-        print("{}: ({}) {}".format(s_name, city.id, city.name))
+    for city, state in res:
+        print("{}: ({}) {}".format(state.name, city.id, city.name))
 
     session.close()
