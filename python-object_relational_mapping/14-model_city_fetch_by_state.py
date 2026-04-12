4b81275c4ab61bbbc8433dac89cc382f10aa5e70
mvprkk-dot <limurad42@gmail.com>
2026-04-12 14:44:14 +0000

    Final fix for Task 14: Resolve E501 and output format

--- python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -8,15 +8,16 @@ from sqlalchemy.orm import sessionmaker
 
 
 if __name__ == "__main__":
-    # Arqumentləri birbaşa götürürük, heç bir sabit parol yazmırıq
+    # E501 xətası (sətir uzunluğu) olmasın deyə qısa dəyişənlər
     u, p, d = sys.argv[1], sys.argv[2], sys.argv[3]
-    uri = 'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(u, p, d)
-    engine = create_engine(uri, pool_pre_ping=True)
+    # Port və localhost-u sətiri qısaltmaq üçün kənara çıxarırıq
+    db = 'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(u, p, d)
+    engine = create_engine(db, pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    # Bütün sorğunu tək sətirdə yazırıq ki, E127 xətası fiziki olaraq QEYRİ-MÜMKÜN olsun
-    res = session.query(City, State).join(State, City.state_id == State.id).order_by(City.id).all()
+    # Join istifadə edərək tək sətirdə bütün şəhərləri və ştatları gətiririk
+    res = session.query(City, State).join(State).order_by(City.id).all()
 
     for city, state in res:
         print("{}: ({}) {}".format(state.name, city.id, city.name))
