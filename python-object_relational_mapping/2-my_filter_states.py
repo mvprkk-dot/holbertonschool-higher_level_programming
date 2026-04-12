46a9f158df7bd97228b25e1e2ef456000e5b739f
mvprkk-dot <limurad42@gmail.com>
2026-04-12 14:10:16 +0000

    Fix pycodestyle W293 whitespace errors

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 """
-Displays all values in the states table where name matches the argument.
-Safe from MySQL injections.
+Lists all values in the states table of hbtn_0e_0_usa
+where name matches the argument.
 """
 import MySQLdb
 import sys
 
+
 if __name__ == "__main__":
-    # Verilənlər bazasına qoşulma
     db = MySQLdb.connect(
         host="localhost",
         port=3306,
@@ -15,15 +15,11 @@ if __name__ == "__main__":
         passwd=sys.argv[2],
         db=sys.argv[3]
     )
-    
     cur = db.cursor()
-    # SQL Injection-dan qorunmaq üçün '%s' formatından istifadə edirik
     query = "SELECT * FROM states WHERE name LIKE BINARY %s ORDER BY id ASC"
     cur.execute(query, (sys.argv[4],))
-    
     rows = cur.fetchall()
     for row in rows:
         print(row)
-        
     cur.close()
     db.close()
