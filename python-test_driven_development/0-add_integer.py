b4cd390cdeed767031c449ac9f6a8ece78f07872
mvprkk-dot <limurad42@gmail.com>
2026-04-12 13:06:59 +0000

    Fix NaN and Infinity conversion error

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -10,7 +10,7 @@ def add_integer(a, b=98):
         b: the second integer.
 
     Raises:
-        TypeError: if a or b are not integers or floats.
+        TypeError: if a or b are not integers or floats, or if they are NaN/Inf.
 
     Returns:
         The sum of the two integers.
@@ -19,4 +19,11 @@ def add_integer(a, b=98):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
+
+    # NaN və ya Infinity yoxlaması
+    if a != a or a == float('inf') or a == float('-inf'):
+        raise TypeError("a must be an integer")
+    if b != b or b == float('inf') or b == float('-inf'):
+        raise TypeError("b must be an integer")
+
     return int(a) + int(b)
