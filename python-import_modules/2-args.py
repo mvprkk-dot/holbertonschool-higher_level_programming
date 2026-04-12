d95031d83319981f237b227a148231d7b25672cd
mvprkk-dot <limurad42@gmail.com>
2026-04-12 11:34:16 +0000

    Fix pycodestyle whitespace

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -4,13 +4,11 @@ import sys
 if __name__ == "__main__":
     argv = sys.argv[1:]
     count = len(argv)
-    
     if count == 0:
         print("0 arguments.")
     elif count == 1:
         print("1 argument:")
     else:
         print("{} arguments:".format(count))
-        
     for i, arg in enumerate(argv, 1):
         print("{}: {}".format(i, arg))
