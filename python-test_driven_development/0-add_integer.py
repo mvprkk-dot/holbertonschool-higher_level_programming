05a972d8b9a08b31e153672aa7ce1263ce330df2
mvprkk-dot <limurad42@gmail.com>
2026-04-12 13:05:06 +0000

    Initial commit for python-test_driven_development

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Module for add_integer method."""
+
+
+def add_integer(a, b=98):
+    """Adds two integers.
+
+    Args:
+        a: the first integer.
+        b: the second integer.
+
+    Raises:
+        TypeError: if a or b are not integers or floats.
+
+    Returns:
+        The sum of the two integers.
+    """
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+    return int(a) + int(b)
