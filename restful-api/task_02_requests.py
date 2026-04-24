bad2b8a03e4446365b17cfd2fd92ae3d86b1a899
mvprkk-dot <limurad42@gmail.com>
2026-04-24 23:10:49 +0400

    Update task_02_requests.py

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -1,4 +1,32 @@
-from task_02_requests import fetch_and_print_posts, fetch_and_save_posts
+import requests
+import csv
 
-fetch_and_print_posts()
-fetch_and_save_posts()
+def fetch_and_print_posts():
+    url = "https://jsonplaceholder.typicode.com/posts"
+    response = requests.get(url)
+    print(f"Status Code: {response.status_code}")
+    if response.status_code == 200:
+        posts = response.json()
+        for post in posts:
+            print(post.get('title'))
+
+def fetch_and_save_posts():
+    url = "https://jsonplaceholder.typicode.com/posts"
+    response = requests.get(url)
+    if response.status_code == 200:
+        posts = response.json()
+        # Lazım olan datanı seçirik
+        data_to_save = []
+        for post in posts:
+            data_to_save.append({
+                'id': post.get('id'),
+                'title': post.get('title'),
+                'body': post.get('body')
+            })
+        
+        # CSV faylına yazırıq
+        with open('posts.csv', 'w', newline='', encoding='utf-8') as f:
+            fieldnames = ['id', 'title', 'body']
+            writer = csv.DictWriter(f, fieldnames=fieldnames)
+            writer.writeheader()
+            writer.writerows(data_to_save)
