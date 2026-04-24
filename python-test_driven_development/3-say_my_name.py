28dc2a0608457a71df6e1bf2098123b54a7b8b97
mvprkk-dot <limurad42@gmail.com>
2026-04-24 22:02:25 +0400

    Update 3-say_my_name.py

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,30 +1,16 @@
 #!/usr/bin/python3
-
 """
-This module provides a function that divides all elements of a matrix.
+Bu modul say_my_name funksiyasını təqdim edir.
 """
 
 
 def say_my_name(first_name, last_name=""):
     """
-    Print full name.
-
-    Parameters:
-        irst_name: string of first name
-        last_name: string of last name
-
-    Returns:
-        full name: first name and last name combine
-
-    Raises:
-        TypeError: if the type is not string
-
+    Ad və soyadı çap edən funksiya.
     """
-    if first_name =="" and last_name == "":
-             raise TypeError("say_my_name() missing 1 required"
-                             "positional argument: 'first_name'")
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
-    print(f"My name is {first_name} {last_name}")
+    
+    print("My name is {} {}".format(first_name, last_name))
