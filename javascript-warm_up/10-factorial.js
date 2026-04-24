8a92ffd7c08cd756453b5c6209ec0fb07047676f
mvprkk-dot <limurad42@gmail.com>
2026-04-24 19:28:16 +0400

    Refactor factorial function and input handling

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -1,14 +1,10 @@
 #!/usr/bin/node
-// JS Script
 
 function factorial (num) {
-  if (isNaN(num) || num <= 1) {
-    return 1;
-  } else {
+  if (num > 0) {
     return num * factorial(num - 1);
   }
+  return 1;
 }
 
-let num = parseInt(process.argv[2]);
-
-console.log(factorial(num));
+console.log(factorial(Number(process.argv[2])));
