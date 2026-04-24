9f700fbed4b6f2474012a8f7b07b86dee05a0144
mvprkk-dot <limurad42@gmail.com>
2026-04-24 22:17:34 +0400

    Update 3-say_my_name.py

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,16 +1,11 @@
 #!/usr/bin/python3
-"""
-Bu modul say_my_name funksiyasını təqdim edir.
-"""
+"""Modulun sənədləşdirilməsi bura yazılır"""
 
 
 def say_my_name(first_name, last_name=""):
-    """
-    Ad və soyadı çap edən funksiya.
-    """
+    """Funksiyanın sənədləşdirilməsi"""
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
-    
     print("My name is {} {}".format(first_name, last_name))
