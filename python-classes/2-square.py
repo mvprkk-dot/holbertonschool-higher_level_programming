fc26d77b856f5162a61bc98f78a3744bf28af311
mvprkk-dot <limurad42@gmail.com>
2026-04-25 18:06:57 +0400

    Update 2-square.py

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,13 +1,23 @@
 #!/usr/bin/python3
+"""Kvadratı təyin edən modul."""
+
+
 class Square:
-    """ A class that defines a square by its size
-    """
+    """Kvadratı təmsil edən klas."""
+
     def __init__(self, size=0):
-        """ Method to initialize the square object
+        """
+        Yeni bir Square yaradır.
+
+        Args:
+            size (int): Kvadratın ölçüsü. Susmaya görə 0-dır.
+
+        Raises:
+            TypeError: Əgər size integer deyilsə.
+            ValueError: Əgər size 0-dan kiçikdirsə.
         """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
-        elif size < 0:
+        if size < 0:
             raise ValueError("size must be >= 0")
-        else:
-            self.__size = int(size)
+        self.__size = size
