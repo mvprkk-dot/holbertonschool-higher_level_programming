b9d46261c416fa8ec80e22f4e4029c63be9aa419
mvprkk-dot <limurad42@gmail.com>
2026-04-25 18:08:58 +0400

    Update 5-square.py

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -1,47 +1,55 @@
 #!/usr/bin/python3
-class Square:
-    """ A class that defines a square by its size
-    """
-    def __init__(self, size=0):
-        """ Method to initialize the square object
-        """
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        elif size < 0:
-            raise ValueError("size must be >= 0")
-        else:
-            self.__size = size
+"""Kvadratı təyin edən modul."""
 
-    def area(self):
-        """ Method that returns the square are of the object
+
+class Square:
+    """Kvadratı təmsil edən klas."""
+
+    def __init__(self, size=0):
         """
-        return (self.__size ** 2)
+        Yeni bir Square yaradır.
+
+        Args:
+            size (int): Kvadratın ölçüsü.
+        """
+        self.size = size
 
     @property
     def size(self):
-        """ Method to returns the size value
-        """
+        """Kvadratın ölçüsünü götürmək (Getter)."""
         return self.__size
 
     @size.setter
     def size(self, value):
-        """ Method to set the size value of the square object
+        """
+        Kvadratın ölçüsünü təyin etmək (Setter).
+
+        Args:
+            value (int): Yeni ölçü dəyəri.
+
+        Raises:
+            TypeError: Əgər value integer deyilsə.
+            ValueError: Əgər value 0-dan kiçikdirsə.
         """
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
-        elif value < 0:
+        if value < 0:
             raise ValueError("size must be >= 0")
-        else:
-            self.__size = value
+        self.__size = value
+
+    def area(self):
+        """
+        Kvadratın sahəsini hesablayır.
+
+        Returns:
+            Sahə dəyəri.
+        """
+        return self.__size * self.__size
 
     def my_print(self):
-        """ Method that prints a # square according
-        to the size value
-        """
-        if not self.__size:
-            print()
+        """Kvadratı '#' simvolları ilə standart çıxışa çap edir."""
+        if self.__size == 0:
+            print("")
         else:
             for i in range(self.__size):
-                for j in range(self.__size):
-                    print("#", end='')
-                print()
+                print("#" * self.__size)
