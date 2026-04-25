ae77ab432390a3aca3243ce0f4c33431e6fcd0d3
mvprkk-dot <limurad42@gmail.com>
2026-04-25 18:21:49 +0400

    Update 3-rectangle.py

--- python-more_classes/3-rectangle.py
+++ python-more_classes/3-rectangle.py
@@ -1,60 +1,70 @@
 #!/usr/bin/python3
-"""Düzbucaqlını təyin edən modul."""
+"""Rectangle module.
+
+This module contains a class that defines a rectangle.
+
+"""
 
 
-class Rectangle:
-    """Düzbucaqlını təmsil edən klas."""
+class Rectangle():
+    """Defines a rectangle."""
 
     def __init__(self, width=0, height=0):
-        """Yeni bir düzbucaqlı yaradır."""
+        """Sets the necessary attributes for the Rectangle object.
+
+        Args:
+            width (int): the width of the rectangle.
+            height (int): the height of the rectangle.
+        """
         self.width = width
         self.height = height
 
+    def __str__(self):
+        """Sets the print behavior of the Rectangle object."""
+        rectangle = ""
+
+        if self.__width > 0 and self.__height > 0:
+            for y in range(self.__height):
+                rectangle += '#' * self.__width + '\n'
+
+        return rectangle[:-1]
+
     @property
     def width(self):
-        """Eni götürmək üçün getter."""
+        """Get or set the width of the rectangle."""
         return self.__width
 
     @width.setter
     def width(self, value):
-        """Eni təyin etmək üçün setter."""
-        if not isinstance(value, int):
+        if type(value) is int:
+            if value >= 0:
+                self.__width = value
+            else:
+                raise ValueError("width must be >= 0")
+        else:
             raise TypeError("width must be an integer")
-        if value < 0:
-            raise ValueError("width must be >= 0")
-        self.__width = value
 
     @property
     def height(self):
-        """Hündürlüyü götürmək üçün getter."""
+        """Get or set the height of the rectangle."""
         return self.__height
 
     @height.setter
     def height(self, value):
-        """Hündürlüyü təyin etmək üçün setter."""
-        if not isinstance(value, int):
+        if type(value) is int:
+            if value >= 0:
+                self.__height = value
+            else:
+                raise ValueError("height must be >= 0")
+        else:
             raise TypeError("height must be an integer")
-        if value < 0:
-            raise ValueError("height must be >= 0")
-        self.__height = value
 
     def area(self):
-        """Düzbucaqlının sahəsini qaytarır."""
+        """Returns the current rectangle area."""
         return self.__width * self.__height
 
     def perimeter(self):
-        """Düzbucaqlının perimetrini qaytarır."""
-        if self.__width == 0 or self.__height == 0:
+        """Returns the current rectangle perimeter."""
+        if self.__width is 0 or self.__height is 0:
             return 0
-        return 2 * (self.__width + self.__height)
-
-    def __str__(self):
-        """Düzbucaqlını '#' simvolu ilə vizual təmsil edir."""
-        if self.__width == 0 or self.__height == 0:
-            return ""
-        rect = [("#" * self.__width) for _ in range(self.__height)]
-        return "\n".join(rect)
-
-    def __repr__(self):
-        """Obyektin rəsmi sətir təmsilini qaytarır."""
-        return "Rectangle({:d}, {:d})".format(self.__width, self.__height)
+        return self.__width * 2 + self.__height * 2
